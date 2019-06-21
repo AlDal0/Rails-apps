aa005a7f0a8996b92432cce92eb81f903f9d34d7
Al Dal <alan.daligault@braintreepayments.com>
2019-06-22 00:48:37 +0200

    Routes correction

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,2 +1,19 @@
 class WebhooksController < ApplicationController
+  def show
+    @webhooks = Webhook.all
+    render 'show.html.erb'
+  end
+  def webhook
+  webhook_notification = gateway.webhook_notification.parse(
+      params["bt_signature"],
+      params["bt_payload"]
+    )
+    webhook = Webhook.new(
+      :kind => webhook_notification.kind,
+      :timestamp => webhook_notifaction.timestamp
+     )
+    webhook.save
+  return 200
+  render 'webhook.html.erb'
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,6 @@
 Rails.application.routes.draw do
 post '/webhook' => 'webhooks#webhook'
 get '/show' => 'webhooks#show'
-  def show
-    @webhooks = Webhook.all
-    render 'show.html.erb'
-  end
-  def webhook
-  webhook_notification = gateway.webhook_notification.parse(
-      params["bt_signature"],
-      params["bt_payload"]
-    )
-    webhook = Webhook.new(
-      :kind => webhook_notification.kind,
-      :timestamp => webhook_notifaction.timestamp
-     )
-    webhook.save
-  return 200
-  render 'webhook.html.erb'
-end
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
