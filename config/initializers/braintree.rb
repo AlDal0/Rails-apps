3c790caf3f789771a17758d0e51072f70b0142f4
Al Dal <alan.daligault@braintreepayments.com>
2019-06-21 22:59:39 +0200

    first commit

---
+++ config/initializers/braintree.rb
@@ -0,0 +1,4 @@
+Braintree::Configuration.environment = :sandbox
+Braintree::Configuration.merchant_id = ENV['y8rz8vkzpyq5nm8y']
+Braintree::Configuration.public_key = ENV['95cw6j42qg8kqh2c']
+Braintree::Configuration.private_key = ENV['6d39c4f35428ae3b53b3ca16afb66947']
---
+++ config/routes.rb
@@ -0,0 +1,20 @@
+Rails.application.routes.draw do
+  def show
+    @webhooks = Webhook.all
+    render 'show.html.erb'
+  end
+  def webhook
+  webhook_notification = gateway.webhook_notification.parse(
+      params["bt_signature"],
+      params["bt_payload"]
+    )
+    webhook = Webhook.new(
+      :kind => webhook_notification.kind,
+      :timestamp => webhook_notifaction.timestamp
+     )
+    webhook.save
+  return 200
+  render 'webhook.html.erb'
+end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
