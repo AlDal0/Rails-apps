a18bbfb464fd6301946df931bc05ecd84a536d43
Al Dal <alan.daligault@braintreepayments.com>
2019-06-26 17:24:23 +0200

    test controller hello world

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 post '/webhook' => 'webhooks#webhook'
 get '/show' => 'webhooks#show'
-  
+get '/hello_world' => 'application#hello_world'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
